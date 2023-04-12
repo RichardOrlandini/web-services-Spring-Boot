83b1f300e5a3e2898f517c3873e62d21332b1686
RichardOrlandini <richardorlandini09@gmail.com>
2023-04-12 18:01:56 -0300

    Service layer, component registration

---
+++ src/main/java/com/richardorlandini/curse/services/UserService.java
@@ -0,0 +1,26 @@
+package com.richardorlandini.curse.services;
+
+import com.richardorlandini.curse.entities.User;
+import com.richardorlandini.curse.repositoryes.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired //spring vai instanciar automaticamene fazendo a injecão da dependencia
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+       Optional<User> obj = repository.findById(id);
+       return obj.get();//retornando um obj do tipo user
+    }
+
+
+}
