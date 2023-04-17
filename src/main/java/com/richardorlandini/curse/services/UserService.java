82b834ba51a07832169c3ea845ae22b58165b3a5
RichardOrlandini <richardorlandini09@gmail.com>
2023-04-17 14:46:51 -0300

    Exception handling - delete

--- src/main/java/com/richardorlandini/curse/services/UserService.java
+++ src/main/java/com/richardorlandini/curse/services/UserService.java
@@ -2,8 +2,11 @@ package com.richardorlandini.curse.services;
 
 import com.richardorlandini.curse.entities.User;
 import com.richardorlandini.curse.repositories.UserRepository;
+import com.richardorlandini.curse.services.exceptions.DatabaseException;
 import com.richardorlandini.curse.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +31,13 @@ public class UserService {
        return repository.save(obj); // o salve por padrão retorna um obj salvo
     }
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
